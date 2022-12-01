864f07f747e5c4584a579eaef300215e1a21de50
Markus Cisler <hello@markuscisler.com>
2022-12-01 18:13:43 +0100

    Finish day 1 part 2

--- 2022/1/main.go
+++ 2022/1/main.go
@@ -2,30 +2,63 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"sort"
 	"strings"
 
 	"github.com/bl1nk/adventofcode/x"
 )
 
 func main() {
-	input := x.ReadString("./input.txt")
-	fmt.Println(mostCalories(input))
+	input := x.ReadString("./2022/1/input.txt")
+	elves := foldCalories(input)
+
+	mostCalories, err := sumN(1, elves)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	fmt.Printf("Part 1: The elf carrying the most calories carries %d calories.\n", mostCalories)
+
+	topThree, err := sumN(3, elves)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	fmt.Printf("Part 2: The three elves carrying the most calories carry %d calories.\n", topThree)
+
 }
 
-func mostCalories(input string) int {
-	lines := strings.Split(input, "\n")
-	var most int
-	var sum int
-	for _, line := range lines {
+func foldCalories(in string) []int {
+	var elf int
+	elves := []int{0}
+	for _, line := range strings.Split(strings.TrimSpace(in), "\n") {
 		if line == "" {
-			if most < sum {
-				most = sum
-			}
-			sum = 0
-		} else {
-			i := x.MustAtoi(line)
-			sum += i
+			elf++
+			elves = append(elves, 0)
+			continue
 		}
+
+		cal := x.MustAtoi(line)
+		elves[elf] += cal
 	}
-	return most
+
+	sort.Ints(elves)
+	return elves
+}
+
+func sumN(n int, ints []int) (int, error) {
+	if n > len(ints) {
+		return 0, fmt.Errorf("got a list of %d ints, cannot sum %d elements", len(ints), n)
+	}
+	toSum := ints[len(ints)-n:]
+
+	s := 0
+	for _, i := range toSum {
+		s += i
+	}
+	return s, nil
+
 }
--- 2022/1/main_test.go
+++ 2022/1/main_test.go
@@ -6,10 +6,31 @@ import (
 	"github.com/bl1nk/adventofcode/x"
 )
 
-func TestMain(t *testing.T) {
-	calories := x.ReadString("./testdata/example.txt")
-	actual := mostCalories(calories)
+func TestPart1(t *testing.T) {
+	in := x.ReadString("./testdata/example.txt")
+
+	elves := foldCalories(in)
+	actual, err := sumN(1, elves)
+	if err != nil {
+		t.Fatal(err)
+	}
+
 	expected := 24000
+	if actual != expected {
+		t.Errorf("expected %d calories, got %d", expected, actual)
+	}
+}
+
+func TestPart2(t *testing.T) {
+	in := x.ReadString("./testdata/example.txt")
+
+	elves := foldCalories(in)
+	actual, err := sumN(3, elves)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expected := 45000
 	if actual != expected {
 		t.Errorf("expected %d calories, got %d", expected, actual)
 	}
