215f599ad7f3ed4df1377a34abb608254f4c7cff
Markus Cisler <hello@markuscisler.com>
2022-12-04 23:01:28 +0100

    Solve day 4 part 2

--- 2022/4/main.go
+++ 2022/4/main.go
@@ -21,20 +21,21 @@ func main() {
 	}
 
 	fmt.Printf("Part 1: One assignment pair fully contains the other %d times\n", part1)
+
+	part2, err := solvePart2(strings.TrimSpace(string(in)))
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	fmt.Printf("Part 2: One assignment pair overlaps another %d times\n", part2)
 }
 
 func solvePart1(in string) (int, error) {
 	lines := strings.Split(in, "\n")
 	var sum int
 	for _, line := range lines {
-		sections := strings.Split(line, ",")
-
-		section1, err := parseSections(sections[0])
-		if err != nil {
-			return 0, err
-		}
-
-		section2, err := parseSections(sections[1])
+		section1, section2, err := parseSections(line)
 		if err != nil {
 			return 0, err
 		}
@@ -47,20 +48,49 @@ func solvePart1(in string) (int, error) {
 	return sum, nil
 }
 
-func parseSections(pair string) (section, error) {
-	boundaries := strings.Split(pair, "-")
+func solvePart2(in string) (int, error) {
+	lines := strings.Split(in, "\n")
+	var sum int
+	for _, line := range lines {
+		section1, section2, err := parseSections(line)
+		if err != nil {
+			return 0, err
+		}
 
-	from, err := strconv.Atoi(boundaries[0])
-	if err != nil {
-		return section{}, err
+		if section1.overlaps(section2) {
+			sum++
+		}
 	}
 
-	to, err := strconv.Atoi(boundaries[1])
+	return sum, nil
+}
+
+func parseSections(line string) (section, section, error) {
+	pair := strings.Split(line, ",")
+
+	s1 := strings.Split(pair[0], "-")
+	from1, err := strconv.Atoi(s1[0])
 	if err != nil {
-		return section{}, err
+		return section{}, section{}, err
 	}
 
-	return section{from: from, to: to}, nil
+	to1, err := strconv.Atoi(s1[1])
+	if err != nil {
+		return section{}, section{}, err
+	}
+
+	s2 := strings.Split(pair[1], "-")
+	from2, err := strconv.Atoi(s2[0])
+	if err != nil {
+		return section{}, section{}, err
+	}
+
+	to2, err := strconv.Atoi(s2[1])
+	if err != nil {
+		return section{}, section{}, err
+	}
+
+	return section{from: from1, to: to1}, section{from: from2, to: to2}, nil
 }
 
 type section struct {
@@ -68,6 +98,10 @@ type section struct {
 	to   int
 }
 
-func (s *section) contains(other section) bool {
+func (s section) contains(other section) bool {
 	return s.from <= other.from && other.to <= s.to
+}
+
+func (s section) overlaps(other section) bool {
+	return s.to >= other.from && s.from <= other.to
 }
--- 2022/4/main_test.go
+++ 2022/4/main_test.go
@@ -22,3 +22,42 @@ func TestPart1(t *testing.T) {
 		t.Fatalf("expected %d, got %d", expected, actual)
 	}
 }
+
+func TestPart2(t *testing.T) {
+	in, err := os.ReadFile("./testdata/example.txt")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	actual, err := solvePart2(strings.TrimSpace(string(in)))
+	if err != nil {
+		t.Fatal(err)
+	}
+	expected := 4
+
+	if actual != expected {
+		t.Errorf("expected %d, got %d", expected, actual)
+	}
+
+	for _, s := range []string{"2-4,6-8", "2-3,4-5"} {
+		s1, s2, err := parseSections(s)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if s1.overlaps(s2) || s2.overlaps(s1) {
+			t.Errorf("s1 (%#v) and s2 (%#v) should not overlap", s1, s2)
+		}
+	}
+	for _, s := range []string{"5-7,7-9", "2-8,3-7", "6-6,4-6", "2-6,4-8"} {
+		s1, s2, err := parseSections(s)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if !s1.overlaps(s2) || !s2.overlaps(s1) {
+			t.Errorf("s1 (%#v) and s2 (%#v) should overlap", s1, s2)
+		}
+
+	}
+}
